4856f0dd7b83430cea8105c9ec131d7bc806a7e4
warriorcurio <ayush.bhatnagar05@gmail.com>
2022-11-15 00:33:16 +0000

    LS buttons placed

--- src/LButton.cpp
+++ src/LButton.cpp
@@ -12,6 +12,7 @@ LButton::LButton(int x, int y, int fontSize, std::string bgColours[3], std::stri
         mH = h;
     }
     mClickable = true;
+    mCurFrame = BUTTON_MOUSE_OUT;
     char svg[300];
     sprintf(svg, "<svg width='%d' height='%d'><rect y='0' rx='5' ry='5' width='%d' height='%d' style='fill:%s' /><rect y='%d' rx='5' ry='5' width='%d' height='%d' style='fill:%s' /><rect y='%d' rx='5' ry='5' width='%d' height='%d' style='fill:%s' /><rect y='%d' rx='5' ry='5' width='%d' height='%d' style='fill:#3F3F3F' /></svg>", mW, mH*4, mW, mH, bgColours[0].c_str(), mH, mW, mH, bgColours[1].c_str(), mH*2, mW, mH, bgColours[2].c_str(), mH*3, mW, mH);
     mTexture.loadFromSVG(svg);
@@ -68,7 +69,7 @@ void LButton::render()
         clip = {0, 3 * mTexture.getHeight() / 4, mTexture.getWidth(), mTexture.getHeight() / 4};
     }
     mTexture.render(mX, mY, &clip);
-    mLabelTexture.render(mX + 5, mY + 5);
+    mLabelTexture.render(mX + (mW - mLabelTexture.getWidth()) / 2, mY + (mH - mLabelTexture.getHeight()) / 2);
 }
 void LButton::setPos(int x, int y)
 {
--- src/LLevelSelect.cpp
+++ src/LLevelSelect.cpp
@@ -12,17 +12,17 @@ void levelSelectBackCall()
 
 bool levelSelectLoadMedia()
 {
-    levelSelectButtons[LEVELSELECT_BUTTON_BACK] = new LButton(10, 1020, 40, levelSelectButtonBackgroundColours, "Back", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_ONE] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_TWO] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_THREE] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_FOUR] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_FIVE] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_SIX] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_SEVEN] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_EIGHT] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_NINE] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
-    levelSelectButtons[LEVELSELECT_BUTTON_TEN] = new LButton(0, 0, 50, levelSelectButtonBackgroundColours, "1", levelSelectButtonTextColour, &levelSelectBackCall);
+    levelSelectButtons[LEVELSELECT_BUTTON_BACK]  = new LButton(  10, 1020, 40, levelSelectButtonBackgroundColours, "Back", levelSelectButtonTextColour, &levelSelectBackCall);
+    levelSelectButtons[LEVELSELECT_BUTTON_ONE]   = new LButton( 485,  335, 90, levelSelectButtonBackgroundColours,  "1", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_TWO]   = new LButton( 680,  335, 90, levelSelectButtonBackgroundColours,  "2", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_THREE] = new LButton( 885,  335, 90, levelSelectButtonBackgroundColours,  "3", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_FOUR]  = new LButton(1085,  335, 90, levelSelectButtonBackgroundColours,  "4", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_FIVE]  = new LButton(1285,  335, 90, levelSelectButtonBackgroundColours,  "5", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_SIX]   = new LButton( 485,  595, 90, levelSelectButtonBackgroundColours,  "6", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_SEVEN] = new LButton( 680,  595, 90, levelSelectButtonBackgroundColours,  "7", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_EIGHT] = new LButton( 885,  595, 90, levelSelectButtonBackgroundColours,  "8", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_NINE]  = new LButton(1085,  595, 90, levelSelectButtonBackgroundColours,  "9", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
+    levelSelectButtons[LEVELSELECT_BUTTON_TEN]   = new LButton(1285,  595, 90, levelSelectButtonBackgroundColours, "10", levelSelectButtonTextColour, &levelSelectBackCall, 150, 150);
     return true;
 }
 void levelSelectHandleEvent(SDL_Event* e)
